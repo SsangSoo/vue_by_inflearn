4614501416b952328208eb57ca46a1549b430d6c
SsangSoo <kss10080@naver.com>
2023-11-17 00:42:17 +0900

    OK

--- learning-vue3/src/App.vue
+++ learning-vue3/src/App.vue
@@ -1,11 +1,28 @@
 <template>
-	<div></div>
+	<div>
+		<ul>
+			<template v-for="(item, index) in eventItems" :key="item.id">
+				<!--				<li v-if="item.id % 2 === 0">-->
+				<li>ID: {{ item.id }} 인덱스: {{ index }},{{ item.message }}</li>
+			</template>
+		</ul>
+	</div>
 </template>
 
 <script>
+import { computed, reactive } from 'vue';
+
 export default {
 	setup() {
-		return {};
+		const items = reactive([
+			{ id: 1, message: 'Java' },
+			{ id: 2, message: 'HTML' },
+			{ id: 3, message: 'CSS' },
+			{ id: 4, message: 'JavaScript' },
+		]);
+
+		const eventItems = computed(() => items.filter(item => item.id % 2 === 0));
+		return { items, eventItems };
 	},
 };
 </script>
