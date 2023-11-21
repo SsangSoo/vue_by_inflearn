33e6bfac9d3b366da88b60edabf1c8cadcedd61f
SsangSoo <kss10080@naver.com>
2023-11-22 01:02:07 +0900

    OK

--- learning-vue3/src/App.vue
+++ learning-vue3/src/App.vue
@@ -3,9 +3,28 @@
 </template>
 
 <script>
+import { ref, watch } from 'vue';
+
 export default {
 	setup() {
-		return {};
+		const x = ref(0);
+		const y = ref(0);
+
+		// Object타입의 반응형 테이터 감지
+
+		// watch(
+		// 	() => x.value + y.value,
+		// 	(sum, oldValue) => {
+		// 		console.log('sum : ', sum);
+		// 		console.log('oldValue : ', oldValue);
+		// 	},
+		// );
+
+		// 배열 타입으오
+		watch([x, y], ([newX, newY]) => {
+			console.log(newX, newY);
+		});
+		return { x, y };
 	},
 };
 </script>
