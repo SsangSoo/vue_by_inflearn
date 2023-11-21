618897784ad2de0ed6ebc55d5ec028d82c282739
SsangSoo <kss10080@naver.com>
2023-11-22 07:45:55 +0900

    14분 46초~

--- learning-vue3/src/App.vue
+++ learning-vue3/src/App.vue
@@ -1,5 +1,8 @@
 <template>
-	<div></div>
+	<div>
+		<p>{{ message }}</p>
+		<p>{{ reverseMessage }}</p>
+	</div>
 </template>
 
 <script>
@@ -7,24 +10,33 @@ import { ref, watch } from 'vue';
 
 export default {
 	setup() {
-		const x = ref(0);
-		const y = ref(0);
+		const message = ref('Hello Vue3');
+		const reverseMessage = ref('');
 
-		// Object타입의 반응형 테이터 감지
+		// 최초 즉시실행 다른 방법
+		// const reverseFunction = newValue => {
+		const reverseFunction = () => {
+			console.log('즉시실행함!!');
+			// reverseMessage.value = newValue.split('').reverse().join('');
+			reverseMessage.value = message.value.split('').reverse().join('');
+		};
 
-		// watch(
-		// 	() => x.value + y.value,
-		// 	(sum, oldValue) => {
-		// 		console.log('sum : ', sum);
-		// 		console.log('oldValue : ', oldValue);
-		// 	},
-		// );
+		watch(
+			message,
+			reverseFunction,
+			// newValue => {
+			// 	console.log('즉시실행함!!');
+			// 	reverseMessage.value = newValue.split('').reverse().join('');
+			// },
+			// {
+			// 	immediate: true, // 최초에 즉시실행
+			// },
+		);
 
-		// 배열 타입으오
-		watch([x, y], ([newX, newY]) => {
-			console.log(newX, newY);
-		});
-		return { x, y };
+		// reverseFunction(message.value); // 해당 최초실행은 watch에 의해 실행되는 게 아님. 매개변수를 따로 넣어줌.
+		reverseFunction(); // 위의 라인 다른 방법 매개변수를 없앤다. 18번, 21번 라인 참고
+
+		return { message, reverseMessage, reverseFunction };
 	},
 };
 </script>
---
+++ learning-vue3/src/lectures/15/App_multiple_source_type.vue
@@ -0,0 +1,66 @@
+<template>
+	<div></div>
+</template>
+
+<script>
+import { reactive, ref, watch } from 'vue';
+
+export default {
+	setup() {
+		const x = ref(0);
+		const y = ref(0);
+
+		// Object타입의 반응형 테이터 감지
+		const obj = reactive({
+			count: 0,
+		});
+
+		// watch(
+		// 	() => x.value + y.value,
+		// 	(sum, oldValue) => {
+		// 		console.log('sum : ', sum);
+		// 		console.log('oldValue : ', oldValue);
+		// 	},
+		// );
+
+		// 배열 타입으오
+		// watch([x, y], ([newX, newY]) => {
+		// 	console.log(newX, newY);
+		// });
+		console.log(typeof obj.count);
+		// watch(
+		// 	() => obj.count,
+		// 	(newValue, oldValue) => {
+		// 		console.log('newValue: ', newValue);
+		// 	},
+		// );
+		// watch(obj, (newValue, oldValue) => {
+		// 	console.log('newValue: ', newValue); // 둘 다
+		// 	console.log('oldValue: ', oldValue); // 같은 객체를 가리킨다.
+		// });
+
+		const person = reactive({
+			name: '홍길동',
+			age: 30,
+			hobby: '운동',
+			obj: {
+				count: 0,
+			},
+		});
+
+		// watch(person, newValue => {
+		// 	console.log('newValue: ', newValue);
+		// });
+
+		watch(
+			() => person.obj,
+			newValue => {
+				console.log('newValue: ', newValue);
+			},
+		);
+		return { x, y, obj, person };
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
